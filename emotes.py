8e17002250b5a19eb20aeef7d74812b4281afb3a
Sarahtonein <joemoe1899@gmail.com>
2023-11-22 14:16:25 +1000

    add contributors ID and mappings

--- emotes.py
+++ emotes.py
@@ -1,13 +1,11 @@
 ##If a contributors emoji is detected in chat, react and ping that user every 4 hrs
 
 #ToDO:
-##Add and Remove contributors
 ##Only ping during waking hours
+##Documentation
 
-##Note:
-##Statically defining contributors is not ideal and there should be functionality
-##to add and remove contributors as required based off uid for example
-
+##Static assignment for initial run
+##Functions exist to add, or remove contributors.
 class Contributor:
     def __init__(self, name, uid, utc_start_time, utc_end_time):
         self.name = name
@@ -16,11 +14,28 @@ class Contributor:
         self.utc_start_time = utc_start_time
         self.utc_end_time = utc_end_time
 
-# Example usage:
-contributor1 = Contributor("Sarah", "316765092689608706", 1, 5)
+contributors = [
+    Contributor("Sarahtonein", "316765092689608706", 1, 5),
+    Contributor("Lapras", "395761182939807744", 1, 5),
+    Contributor("Balu", "353572599957291010", 1, 5),
+    Contributor("Pizzacat", "303732860265693185", 1, 5),
+    Contributor("Spaghetto", "406302927884386318", 1, 5),
+    Contributor("Lilgumbo", "368617749041381388", 1, 5),
+    Contributor("Baguette", "548974112131776522", 1, 5),
+    Contributor("Bagelface", "856041684588167188", 1, 5),
+    Contributor("Breeze", "154033306894073856", 1, 5),
+    Contributor("Ploxx95", "406073034160472066", 1, 5)
+]
 
-# Map Emoji_ID to Contributor:
 emoji_id_mapping = {
-    "<:sarah:1176399164154851368>": contributor1,
-    # Add more entries as needed
+    "<:sarah:1176399164154851368>": contributors[0],
+    "<:lap:1110862059647795240>": contributors[1],
+    "<:balu:1110862230343397387>": contributors[2],
+    "<:pizzacat:1110862947145760939>": contributors[3],
+    "<:spag:1110866508017586187>": contributors[4],
+    "<:gumbo:1145946572589383731>": contributors[5],
+    "<:baguette:1113326206947954849>": contributors[6],
+    "<:bagelface:1148723908556632205>": contributors[7],
+    "<:breeze:1113744843575922708>": contributors[8],
+    "<:ploxx:1154103719496003709>": contributors[9]
 }
