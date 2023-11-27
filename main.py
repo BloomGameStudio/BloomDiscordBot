123fbf04bb0c06866ae20440497794d6bdad3b44
Sarahtonein <joemoe1899@gmail.com>
2023-11-27 15:58:51 +1000

    initial event post delay commit

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ async def on_scheduled_event_create(event):
 
     print(f"New scheduled event created: {event.name}")
 
+    # Delay for 5 minutes (300 seconds) before checking for changes
+    await asyncio.sleep(300)
+
+    # Check if there are any changes to the event
+    if has_changes(event):
+        # Get the channel ID from the environment variable
+        channel_id = int(os.getenv('CHANNEL_ID'))
+        general_channel = bot.get_channel(channel_id)
+
+        if general_channel:
+            start_time = event.start_time.replace(tzinfo=timezone.utc)
+            await general_channel.send(f"Updated Event!\n**Event Name:** {event.name}\n**Start Time:** {start_time}")
+        else:
+            print("General channel not found or bot doesn't have permission to send messages.")
+    else:
+        print("No changes to the event.")
+        
 # Command to list stored scheduled events
 @bot.command(name='listevents')
 async def list_events(ctx):
@@ -40,5 +57,16 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
     await load_and_post_events(bot)
 
+# Function to check for changes in the event
+def has_changes(event):
+    stored_event = scheduled_events.get(event.id)
+    if stored_event:
+        return (
+            stored_event['name'] != event.name or
+            stored_event['start_time'] != event.start_time or
+            stored_event['status'] != event.status
+        )
+    else :
+        return True
 # Start the bot
 bot.run(os.getenv('TOKEN'))
