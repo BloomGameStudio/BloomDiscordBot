078f20338934becd779b7caa35c1638cd821336c
Balu <baluproton+gitlab@protonmail.com>
2023-11-28 14:41:42 +0100

    Discord Client Setup and Configuration

--- gov/proposals.py
+++ gov/proposals.py
@@ -2,3 +2,9 @@ import discord
 import textwrap
 import random
 import os
+
+# Discord Client Setup
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
