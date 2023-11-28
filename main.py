f27f1cf3f616736328ade02dc663b57c7900795b
Sarahtonein <joemoe1899@gmail.com>
2023-11-28 14:11:20 +1000

    changed logic to utilize discord more

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-import asyncio
-from updates import *
+from datetime import datetime
+from updates import check_upcoming_events
 
 intents = discord.Intents.all()
 
@@ -13,60 +13,22 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Event handler for on_scheduled_event_create
 @bot.event
 async def on_scheduled_event_create(event):
-    # Store the scheduled event in the dictionary
-    scheduled_events[event.id] = {
-        'name': event.name,
-        'start_time': event.start_time,
-        'status': event.status
-    }
-
-    # Save the updated scheduled events to updates.json
-    save_scheduled_events()
-
     print(f"New scheduled event created: {event.name}")
-
-    # Delay for 5 minutes (300 seconds) before checking for changes
-    await asyncio.sleep(300)
-
-    # Check if there are any changes to the event
-    if has_changes(event):
-        # Get the channel ID from the environment variable
-        channel_id = int(os.getenv('CHANNEL_ID'))
-        general_channel = bot.get_channel(channel_id)
-
-        if general_channel:
-            start_time = event.start_time.replace(tzinfo=timezone.utc)
-            await general_channel.send(f"Updated Event!\n**Event Name:** {event.name}\n**Start Time:** {start_time}")
-        else:
-            print("General channel not found or bot doesn't have permission to send messages.")
-    else:
-        print("No changes to the event.")
-        
-# Command to list stored scheduled events
-@bot.command(name='listevents')
-async def list_events(ctx):
-    if not scheduled_events:
-        await ctx.send("No scheduled events found.")
-    else:
-        event_list = "\n".join(f"{name} - {info['start_time']} - {info['status']}" for name, info in scheduled_events.items())
-        await ctx.send(f"Stored Scheduled Events:\n{event_list}")
 
 # Event to run when the bot is fully connected and ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    await load_and_post_events(bot)
+    guild_id = int(os.getenv("GUILD_ID"))
+    guild = bot.get_guild(guild_id)
+    
+    if guild:
+        check_upcoming_events(guild)
+    else:
+        print(f"Guild not found")
 
-# Function to check for changes in the event
-def has_changes(event):
-    stored_event = scheduled_events.get(event.id)
-    if stored_event:
-        return (
-            stored_event['name'] != event.name or
-            stored_event['start_time'] != event.start_time or
-            stored_event['status'] != event.status
-        )
-    else :
-        return True
+# Load the environment variables
+load_dotenv()
+
 # Start the bot
 bot.run(os.getenv('TOKEN'))
--- updates.py
+++ updates.py
@@ -1,75 +1,37 @@
-import json
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 import os
 import asyncio
 
 load_dotenv()
 
-# JSON Encoding for dealing with datetime format for updates.json
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return super().default(obj)
+class ScheduledEvent:
+    def __init__(self, name, description, start_time):
+        self.name = name
+        self.description = description
+        self.start_time = start_time
+        self.interested_users = []  # Attribute to store interested users
 
-# Load scheduled events from updates.json
-def load_scheduled_events():
-    try:
-        with open('updates.json', 'r') as file:
-            data = json.load(file)
-            # Convert start_time to datetime objects
-            for event_id, event_info in data.items():
-                event_info['start_time'] = datetime.fromisoformat(event_info['start_time'])
-            return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}
+    # Other methods and attributes for the class
 
-# Save scheduled events to updates.json
-def save_scheduled_events():
-    with open('updates.json', 'w') as file:
-        json.dump(scheduled_events, file, indent=4, cls=DateTimeEncoder)
+def check_upcoming_events(guild):
+    current_time = datetime.now().astimezone(timezone.utc)
+    events = guild.scheduled_events
+    upcoming_events = []
 
-# Dictionary to store scheduled events
-scheduled_events = load_scheduled_events()
+    for event in events:
+        event_name = event.name
+        event_description = event.description
+        event_time = event.start_time
+        event_user_count = event.user_count
 
-# Function to cleanup updates.json by removing events that have already occurred
-def cleanup_scheduled_events():
-    global scheduled_events
-    current_time = datetime.utcnow().replace(tzinfo=timezone.utc)
-    # Filter out events that have already occurred
-    scheduled_events = {event_id: event_info for event_id, event_info in scheduled_events.items() if event_info['start_time'] > current_time}
-    # Save the cleaned-up scheduled events to updates.json
-    save_scheduled_events()
+        time_difference = event_time - current_time
+        if time_difference.total_seconds() <= 24 * 3600:
+            # Event is 24 hours or less away
+            upcoming_events.append(event)
+            print(f"Event Name: {event_name}")
+            print(f"Event Description: {event_description}")
+            print(f"Event Start Time: {event_time}")
+            print(f"Interested Users: {(event_user_count)}")
 
-# Function to post event details 24 hours before it begins
-async def post_event_details(bot):
-    print("Checking for scheduled events")
-    print(f'Number of scheduled events: {len(scheduled_events)}')
-
-    for event_id, event_info in scheduled_events.items():
-        start_time = event_info['start_time'].replace(tzinfo=timezone.utc)  # Make start_time timezone-aware
-        current_time = datetime.utcnow().replace(tzinfo=timezone.utc)  # Make current_time timezone-aware
-        # Check if the event is scheduled within the next 24 hours
-        if start_time - current_time <= timedelta(hours=24) and start_time > current_time:
-            print(f"Event '{event_info['name']}' is scheduled within the next 24 hours")
-            channel_id = int(os.getenv('CHANNEL_ID'))
-            # Check if the channel exists and the bot has access to it
-            general_channel = bot.get_channel(channel_id)
-            if general_channel:
-                await general_channel.send(f"Events in 24 hours or less!\n**Event Name:** {event_info['name']}\n**Start Time:** {start_time}")
-            else:
-                print("General channel not found or bot doesn't have permission to send messages.")
-        else:
-            # If the event has already occurred, cleanup the updates.json
-            cleanup_scheduled_events()
-
-# Function to load and post events with a periodic cleanup
-async def load_and_post_events(bot):
-    await bot.wait_until_ready()
-    # Load scheduled events before starting the bot
-    global scheduled_events  # Use the global variable
-    scheduled_events = load_scheduled_events()
-    while not bot.is_closed():
-        await post_event_details(bot)
-        await asyncio.sleep(60 * 60 * 24)  # Sleep for 24 hours before the next iteration
+    return upcoming_events
