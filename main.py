673b229d21bce522a33aca934415903929f18e58
Sarahtonein <joemoe1899@gmail.com>
2023-11-28 15:02:28 +1000

    add task with loop and list events command

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import os
-from datetime import datetime
-from updates import check_upcoming_events
+from datetime import datetime, timedelta
+from updates import check_upcoming_events, format_event
 
 intents = discord.Intents.all()
 
-# Create a bot instance with intents
+load_dotenv()
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Event handler for on_scheduled_event_create
-@bot.event
-async def on_scheduled_event_create(event):
-    print(f"New scheduled event created: {event.name}")
-
-# Event to run when the bot is fully connected and ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     guild_id = int(os.getenv("GUILD_ID"))
     guild = bot.get_guild(guild_id)
-    
+
     if guild:
         check_upcoming_events(guild)
+        # Start the background task to check events automatically every 24 hours
+        daily_check_events.start()
     else:
         print(f"Guild not found")
 
-# Load the environment variables
-load_dotenv()
+#This may have its own issues such as if the bot is restarted.
+@tasks.loop(hours=24)
+async def daily_check_events():
+    guild_id = int(os.getenv("GUILD_ID"))
+    guild = bot.get_guild(guild_id)
 
-# Start the bot
+    if guild:
+        event_list = check_upcoming_events(guild, time_range=24 * 3600)
+        formatted_events = [format_event(event) for event in event_list]
+        formatted_string = "\n\n".join(formatted_events)
+
+        channel_id = int(os.getenv("CHANNEL_ID"))
+        channel = guild.get_channel(channel_id)
+
+        if channel:
+            await channel.send(f"**Upcoming Events in the Next 24 Hours**:\n{formatted_string}")
+        else:
+            print(f"Event channel not found")
+    else:
+        print(f"Guild not found")
+
+@bot.command(name='listevents')
+async def listevents(ctx):
+    guild = ctx.guild
+    event_list = check_upcoming_events(guild)
+    formatted_events = [format_event(event) for event in event_list]
+    formatted_string = "\n\n".join(formatted_events)
+
+    await ctx.send(f"**All Events**:\n{formatted_string}")
+
 bot.run(os.getenv('TOKEN'))
