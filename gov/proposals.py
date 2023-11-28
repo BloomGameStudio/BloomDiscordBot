9877ba59f0fa6cf2a9f4bf8863770d1bd2f56d29
Balu <baluproton+gitlab@protonmail.com>
2023-11-28 14:51:41 +0100

    Initialization Command / Start a Proposal

--- gov/proposals.py
+++ gov/proposals.py
@@ -8,3 +8,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    fmt_proposals = ""
+
+    # Loop over proposals and convert them to str with every proposal name beeing on a newline
+    for proposal in proposals:
+        fmt_proposals += f"📝 {proposal['name']}\n"
+
+    if message.content.startswith("!vote_draft") or message.content.startswith("!v"):
+        msg = f"Would you like to work on an existing draft or a new one? existing drafts are:"
+
+        await message.channel.send(msg)
+
+        for proposal in proposals:
+            await message.channel.send(f"📝 {proposal['name']}")
+
+        await message.channel.send(f"{new_proposal_emoji} New")
