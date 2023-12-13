f83de2136e6dc10c531a45cc47708b1664d76bc8
Sarahtonein <joemoe1899@gmail.com>
2023-12-13 15:12:37 +1000

    add logging to shared

--- shared/shared.py
+++ shared/shared.py
@@ -1,4 +1,6 @@
 import configparser
+import logging
+
 # Load configuration
 config = configparser.ConfigParser()
 config.read('config/config.ini')  
@@ -6,3 +8,7 @@ config.read('config/config.ini')
 current_governance_id = config.getint('ID_START_VALUES', 'governance_id')
 current_budget_id = config.getint('ID_START_VALUES', 'budget_id')
 
+# Set up logging
+logging_level = config.get('Logging', 'level', fallback='INFO')
+numeric_logging_level = getattr(logging, logging_level.upper(), logging.INFO)
+logging.basicConfig(level=numeric_logging_level)
