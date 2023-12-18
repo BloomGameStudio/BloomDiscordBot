f889c9b3301202015ce2baf0a5486c3fb3d84d20
Sarahtonein <joemoe1899@gmail.com>
2023-12-19 09:18:34 +1000

    Load contributors.json correctly

--- emojis/emojis.py
+++ emojis/emojis.py
@@ -1,17 +1,23 @@
-import asyncio
 import json
+import os
 from datetime import datetime
 from pytz import timezone
 
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Construct the path to the contributors.json file
+json_file_path = os.path.join(script_dir, 'contributors.json')
+
 # Load contributors and emoji ID mapping from contributors.json
-with open('contributors.json', 'r') as json_file:
+with open(json_file_path, 'r') as json_file:
     data = json.load(json_file)
     contributors = data["contributors"]
 
 emoji_id_mapping = {emoji: contributor for emoji, contributor in data["emojiIdMapping"].items()}
 
-def add_contributor(name, uid, emoji_id):    
-    new_contributor = {"name": name, "uid": uid}
+def add_contributor(uid, emoji_id):    
+    new_contributor = {"uid": uid}
     contributors.append(new_contributor)
     emoji_id_mapping[emoji_id] = uid  # Use the UID directly as the value in emoji_id_mapping
     update_json_file()
@@ -24,5 +30,5 @@ async def send_dm_once(bot, contributor, message_link):
         await user.send(dm_message)
 
 def update_json_file():
-    with open('contributors.json', 'w') as json_file:
+    with open(json_file_path, 'w') as json_file:
         json.dump({"contributors": contributors or [], "emojiIdMapping": emoji_id_mapping or {}}, json_file)
