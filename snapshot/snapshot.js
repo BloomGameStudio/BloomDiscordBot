84eda427bde9df5cf47332d1cd8f571d06f64bfc
Sarahtonein <joemoe1899@gmail.com>
2023-12-19 08:42:16 +1000

    Using JS for snapshot and subprocess in py to execute

---
+++ snapshot/snapshot.js
@@ -0,0 +1,54 @@
+const { ethers } = require('ethers');
+const snapshot = require('@snapshot-labs/snapshot.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+async function createProposal() {
+  try {
+    const ethAddress = process.env.ETH_ADDRESS;
+    const ethPrivateKey = process.env.ETH_PRIVATE_KEY;
+
+    if (!ethAddress || !ethPrivateKey) {
+      throw new Error('Ethereum address or private key not provided in environment variables');
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth_goerli'); // Replace with your RPC endpoint
+
+    const wallet = new ethers.Wallet(ethPrivateKey, provider);
+
+    const hub = 'https://testnet.hub.snapshot.org';
+
+    // Initialize Snapshot client
+    const client = new snapshot.Client712(hub);
+
+    // Define proposal parameters
+    const currentTime = Math.floor(new Date().getTime() / 1000); // Current time in seconds
+    const oneHourInSeconds = 3600;
+
+    const proposalParams = {
+      space: 'testdao1.eth',
+      type: 'single-choice', // define the voting system
+      title: 'Test proposal using Snapshot.js',
+      body: 'This is the content of the proposal',
+      choices: ['Alice', 'Bob', 'Carol'],
+      start: currentTime,
+      end: currentTime + oneHourInSeconds, // End time is one hour later as an example
+      snapshot: await provider.getBlockNumber(), // Current block number as snapshot
+      network: '1',
+      plugins: JSON.stringify({}),
+      app: 'my-app' // provide the name of your project using this Snapshot.js integration
+    };
+
+    // Submit the proposal
+    const receipt = await client.proposal(wallet, ethAddress, proposalParams);
+
+    // Log the receipt details
+    console.log('Proposal submitted. Receipt:', receipt);
+  } catch (error) {
+    console.error('Error creating proposal:', error);
+  }
+}
+
+// Create the proposal
+createProposal();
--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,33 +1,5 @@
-from web3 import Web3
-from typing import Any, Dict
-import requests
+import subprocess
 
-class Snapshot:
-    def __init__(self, hub_url: str):
-        self.hub_url = hub_url
+js_file_path = './snapshot/snapshot.js'
 
-    def proposal(self, web3: Web3, account: str, proposal_data: Dict[str, Any]) -> Dict[str, Any]:
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        
-        # Construct the payload
-        payload = {
-            'address': account,
-            'msg': proposal_data
-        }
-
-        # Print the payload for debugging
-        print("Payload:", payload)
-
-        # Make an HTTP POST request to the Snapshot API
-        response = requests.post(f'{self.hub_url}', json=payload, headers=headers)
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            receipt = response.json()
-            return receipt
-        else:
-            # Handle the case when the request fails
-            print(f'Failed to create proposal: {response.status_code} - {response.text}')
-            return {'status': 'failed'}
+subprocess.run(['node', js_file_path])
