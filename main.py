05aff936ac2844d9a4ef590a74b23c81fe6079c4
Sarahtonein <joemoe1899@gmail.com>
2023-12-19 13:35:43 +1000

    main func

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import os
 from discord.ext import commands
 from bot.commands import setup_commands
 from bot.events import setup_events
+from updates.updates import load_posted_events
 
-# Discord Config
-intents = discord.Intents.default()
-intents.message_content = True
-intents.reactions = True
-bot = commands.Bot(command_prefix="$", intents=intents)
+def main():
+    # Discord Config
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.reactions = True
+    bot = commands.Bot(command_prefix="$", intents=intents)
 
-# Setup commands and events
-setup_commands(bot)
-setup_events(bot)
+    # Setup commands and events
+    setup_commands(bot)
+    setup_events(bot)
 
-# Run the bot
-bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+    load_posted_events()
+
+    # Run the bot
+    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+
+if __name__ == "__main__":
+    main()
