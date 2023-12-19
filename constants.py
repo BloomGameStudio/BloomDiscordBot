d60a8f43a8e8e34b7026549f4d4da0a6adc463b8
Sarahtonein <joemoe1899@gmail.com>
2023-12-20 09:03:35 +1000

    remove config and declare constants

--- constants.py
+++ constants.py
@@ -1,12 +1,8 @@
-import configparser
 import os
 
-# Read config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-# Access values in the config file
-GUILD_ID = int(config['DISCORD']['GUILD_ID'])
-GENERAL_CHANNEL_ID = int(config['DISCORD']['GENERAL_CHANNEL_ID'])
-FILE_PATH = config['DISCORD']['FILE_PATH']
+# Constants used throughout the bot
+# set the GUILD_ID and GENERAL_CHANNEL_ID to the server and channel you want the bot to post events to
+GUILD_ID = 1119110120522133537
+GENERAL_CHANNEL_ID = 1176395644261367828
+FILE_PATH = './events/posted_events.json'
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
