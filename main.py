d225fe3f186508395f111ce01fcfd9d800ba1234
Sarahtonein <joemoe1899@gmail.com>
2023-12-20 15:13:43 +1000

    load contributors.json in main

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import discord
 from discord.ext import commands
 from emotes.commands import setup_contrbitutor_commands
 from emotes.events import setup_contributor_events
-from constants import DISCORD_BOT_TOKEN
+from constants import DISCORD_BOT_TOKEN, FILE_PATH
+import json
 
 def main():
+    # Load contributors and emoji ID mapping from contributors.json
+    with open(FILE_PATH, 'r') as json_file:
+        data = json.load(json_file)
+        contributors = data["contributors"]
+        emoji_id_mapping = {emoji: contributor for emoji, contributor in data["emojiIdMapping"].items()}
+
     # Discord Config
     intents = discord.Intents.default()
     intents.message_content = True
     intents.reactions = True
     bot = commands.Bot(command_prefix="$", intents=intents)
-
+    
     # Setup the emotes discord commands, and events
-    setup_contrbitutor_commands(bot)
-    setup_contributor_events(bot)
+    setup_contrbitutor_commands(bot, contributors, emoji_id_mapping)
+    setup_contributor_events(bot, contributors, emoji_id_mapping)
 
     # Run the bot
     bot.run(DISCORD_BOT_TOKEN)
