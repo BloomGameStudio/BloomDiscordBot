7a86018703e1c9f84825fb63e2964c015becf96b
Sarahtonein <joemoe1899@gmail.com>
2023-12-20 12:53:18 +1000

    working par signing

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -1,21 +1,55 @@
-"""
-Currently we are using subprocess to run the snapshot.js file
-in the snapshot directory. 
+import os
+import time
+import json
+import requests
+from web3 import Web3
+from eth_account.messages import encode_defunct
 
-This is because there is no Python Library for Snapshot, and the JS
-Library appears to utilize signing messages using EIP-712 standard which 
-no library appears to exist for this in Python.
+# Load environment variables
+eth_address = os.getenv('ETH_ADDRESS')
+eth_private_key = os.getenv('ETH_PRIVATE_KEY')
 
-The API requires that the message is signed using EIP-712 standard. More 
-information is here: 
+if not eth_address or not eth_private_key:
+    raise ValueError('Ethereum address or private key not provided in environment variables')
 
-https://snapshot.mirror.xyz/vuManI14DW8u2zhrlskndNgQcXOTbKIelQvkgmxOG2k
+# Initialize Web3
+w3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth_goerli'))  # Replace with your RPC endpoint
 
-We could try and make this ourselves but it could be a lot of work and
-I am not sure if it is worth it at the moment
-"""
-import subprocess
+# Define proposal parameters
+current_time = int(time.time())  # Current time in seconds
+one_hour_in_seconds = 3600
 
-js_file_path = './snapshot/snapshot.js'
+proposal_params = {
+    'space': 'testdao1.eth',
+    'type': 'single-choice',  # define the voting system
+    'title': 'Test proposal using Python',
+    'body': 'This is the content of the proposal',
+    'choices': ['Alice', 'Bob', 'Carol'],
+    'start': current_time,
+    'end': current_time + one_hour_in_seconds,  # End time is one hour later as an example
+    'snapshot': w3.eth.block_number,  # Current block number as snapshot
+    'network': '1',
+    'plugins': json.dumps({}),
+    'app': 'my-app'  # provide the name of your project using this Snapshot.js integration
+}
 
-subprocess.run(['node', js_file_path])
+# Sign the proposal
+message = encode_defunct(text=json.dumps(proposal_params))
+signed_message = w3.eth.account.sign_message(message, private_key=eth_private_key)
+
+# Submit the proposal
+url = 'https://testnet.hub.snapshot.org/api/message'
+headers = {'Content-Type': 'application/json'}
+data = {
+    'address': eth_address,
+    'msg': signed_message.messageHash.hex(),  # Use the hash of the original message
+    'sig': signed_message.signature.hex(),
+    'version': '2'
+}
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# Log the response details
+if response.status_code == 200:
+    print('Proposal submitted. Response:', response.json())
+else:
+    print('Error creating proposal:', response.text)
