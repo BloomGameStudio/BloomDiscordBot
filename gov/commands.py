c3f14bba1541f7acff83886b3d1e86ab3e83a686
Sarahtonein <joemoe1899@gmail.com>
2023-12-22 14:10:49 +1000

    notes

--- gov/commands.py
+++ gov/commands.py
@@ -2,6 +2,13 @@ from .command_operations import handle_votedraft, handle_publishdraft
 from .proposals import proposals
 from constants import new_proposal_emoji
 
+"""
+When the bot is initiated the command list below will be loaded so that they can be called.
+The function calls related to the commands are located in command_operations.py
+
+setup_gov_commands is used so that all event commands can be loaded at once. instead of individually.
+"""
+
 def setup_gov_commands(bot):
     @bot.command(name='vote_draft', aliases=['v'], pass_context=True)
     async def votedraft(ctx):
--- gov/events.py
+++ gov/events.py
@@ -3,6 +3,11 @@ from .event_operations import handle_on_message, handle_on_reaction_add
 from constants import new_proposal_emoji
 from .proposals import proposals
 
+"""
+The bot will listen for messages and reactions when the bot is initiated.
+setup_event_events is used so that all event events can be loaded at once. instead of individually.
+"""
+
 def setup_gov_events(bot: commands.Bot):
     @bot.event
     async def on_message(message):
