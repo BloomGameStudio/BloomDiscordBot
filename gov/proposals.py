a5e7c2eb62001217f8e04147384188c8ad730eb5
Sarahtonein <joemoe1899@gmail.com>
2023-12-22 14:08:49 +1000

    Remove duplicate func call

--- gov/proposals.py
+++ gov/proposals.py
@@ -1,8 +1,8 @@
 import textwrap
-import os
 import asyncio
 import subprocess
-from constants import current_budget_id, current_governance_id, GOVERNANCE_BUDGET_CHANNEL_ID, GOVERNANCE_CHANNEL_ID
+from constants import GOVERNANCE_BUDGET_CHANNEL_ID, GOVERNANCE_CHANNEL_ID
+from config.config import current_governance_id, current_budget_id, update_id_values
 
 proposals = []
 
@@ -25,31 +25,23 @@ def get_budget_id():
     return current_budget_id
 
 async def publish_draft(draft, client):
-    #Delete?
-    channel = None  # Initialize channel with None
-
     if draft["type"].lower() == "budget":
         channel_id = int(GOVERNANCE_BUDGET_CHANNEL_ID)
         current_budget_id = get_next_budget_id()
+        update_id_values(current_budget_id, 'budget')  # Update the budget ID in the config file
         title = f"**Bloom Budget Proposal (BBP) #{current_budget_id}: {draft['name']}**"
     else:
         channel_id = int(GOVERNANCE_CHANNEL_ID)
         current_governance_id = get_next_governance_id()
+        update_id_values(current_governance_id, 'governance')  # Update the governance ID in the config file
         title = f"**Bloom Improvement Proposal (BIP) #{current_governance_id}: {draft['name']}**"
-
+        
     channel = client.get_channel(channel_id)
 
     if not channel:
         print("Error: Channel not found.")
         return
 
-    if draft["type"].lower() == "budget":
-        current_budget_id = get_next_budget_id()
-        title = f"**Bloom Budget Proposal (BBP) #{get_budget_id()} {draft['name']}**"
-    else:
-        current_governance_id = get_next_governance_id()
-        title = f"**Topic/Vote {get_governance_id()}: {draft['name']}**"
-  
     # Create the message content using the draft information
     msg = f"""
     {title}
@@ -80,7 +72,7 @@ async def publish_draft(draft, client):
 
     # Start the timer coroutine
     asyncio.create_task(vote_timer(vote_message.id, client, channel_id, title))
-
+    
 async def vote_timer(message_id, client, channel_id, title):
     # Sleep until the vote ends
     await asyncio.sleep(ongoing_votes[message_id]["end_time"] - asyncio.get_event_loop().time())
@@ -100,7 +92,7 @@ async def vote_timer(message_id, client, channel_id, title):
     # Check the result and post it
     result_message = f"Vote for '{title}' has concluded:\n\n"
 
-    if ongoing_votes[message_id]["yes_count"] >= 5: #Set to qurom needed
+    if ongoing_votes[message_id]["yes_count"] >= 1: #Set to qurom needed
         result_message += "The vote passes! :tada:"
         # Call the snapshot creation function
         subprocess.run(['node', './snapshot/wrapper.js', title, ongoing_votes[message_id]['draft']['abstract'], ongoing_votes[message_id]['draft']['background'], 'Yes', 'No', 'Abstain'], check=True)
