39316caa310681efb993b10433e7824a20a99339
sarahtonein <joemoe1899@gmail.com>
2024-01-10 00:25:32 -0500

    config type annotation

--- config/config.py
+++ config/config.py
@@ -1,20 +1,21 @@
 import configparser
 import logging
+from typing import Any
 
 # Load configuration
-config = configparser.ConfigParser()
+config: configparser.ConfigParser = configparser.ConfigParser()
 config.read('config/config.ini')  
 
-current_governance_id = config.getint('ID_START_VALUES', 'governance_id')
-current_budget_id = config.getint('ID_START_VALUES', 'budget_id')
+current_governance_id: int = config.getint('ID_START_VALUES', 'governance_id')
+current_budget_id : int = config.getint('ID_START_VALUES', 'budget_id')
 
 # Set up logging
-logging_level = config.get('Logging', 'level', fallback='INFO')
-numeric_logging_level = getattr(logging, logging_level.upper(), logging.INFO)
+logging_level: str = config.get('Logging', 'level', fallback='INFO')
+numeric_logging_level: int = getattr(logging, logging_level.upper(), logging.INFO)
 logging.basicConfig(level=numeric_logging_level)
 
 # Update values when proposals are submitted.
-def update_id_values(id_value, id_type):
+def update_id_values(id_value: int, id_type: str) -> None:
     config = configparser.ConfigParser()
     config.read('config/config.ini')  # Corrected path
 
