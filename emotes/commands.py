f926af451914eacc80e35597f54efb6485dbe05d
sarahtonein <joemoe1899@gmail.com>
2024-01-10 00:25:50 -0500

    emotes type annotation

--- emotes/commands.py
+++ emotes/commands.py
@@ -1,4 +1,5 @@
-from discord.ext import commands
+from typing import List, Dict, Optional
+from discord.ext import commands, Context
 from emotes.command_operations import list_contributors, remove_contributor, add_contributor
 
 """
@@ -8,15 +9,15 @@ The function calls related to the commands are located in command_operations.py
 setup_contributor_commands is used so that all event commands can be loaded at once. instead of individually.
 """
 
-def setup_contrbitutor_commands(bot: commands.Bot, contributors, emoji_id_mapping):
+def setup_contrbitutor_commands(bot: commands.Bot, contributors: List[Dict[str, str]], emoji_id_mapping: Dict[str, str]) -> None:
     @bot.command(name='contributors')
-    async def listcontributors(ctx):
+    async def listcontributors(ctx: Context) -> None:
         await list_contributors(ctx, emoji_id_mapping)
 
     @bot.command(name='remove_contributor')
-    async def removecontributor(ctx, user_mention=None):
+    async def removecontributor(ctx: Context, user_mention: str) -> None:
         await remove_contributor(ctx, contributors, emoji_id_mapping, user_mention)
     
     @bot.command(name='add_contributor')
-    async def addcontributor(ctx):
+    async def addcontributor(ctx: Context) -> None:
         await add_contributor(ctx, contributors, emoji_id_mapping, bot)
