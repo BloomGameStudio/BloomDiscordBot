f45fa0480d02601f0e151040992d1f7c9fe77ba1
sarahtonein <joemoe1899@gmail.com>
2024-01-09 21:50:01 -0500

    update consts for chan names

--- shared/constants.py
+++ shared/constants.py
@@ -1,10 +1,9 @@
 # Constants used throughout the bot
-# set the GUILD_ID and GENERAL_CHANNEL_ID to the server and channel you want the bot to post events to
-GUILD_ID = 1010707985242271754
-GENERAL_CHANNEL_ID = 1010707988543197246
-GOVERNANCE_CHANNEL_ID = 1094053223867875328
-GOVERNANCE_TALK_CHANNEL_ID = 1108853413455863929
-GOVERNANCE_BUDGET_CHANNEL_ID = 1126294949286133870
+# Channel names are case sensitive and must have the same name in their respective servers
+GENERAL_CHANNEL_ID = 'general'
+GOVERNANCE_TALK_CHANNEL_ID = 'governance-talk'
+GOVERNANCE_CHANNEL_ID = 'governance'
+GOVERNANCE_BUDGET_CHANNEL_ID = 'gov-budget'
 new_proposal_emoji = "💡"
 CONTRIBUTORS_FILE_PATH = './emotes/contributors.json'
 POSTED_EVENTS_FILE_PATH = './events/posted_events.json'
