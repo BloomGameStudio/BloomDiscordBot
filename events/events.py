4848a23b030c580549c904c7b00c59231c63de65
sarahtonein <joemoe1899@gmail.com>
2024-01-15 03:18:39 -0500

    load emoji dict based on server name

--- events/events.py
+++ events/events.py
@@ -3,6 +3,7 @@ from discord import ScheduledEvent
 from discord.ext import commands
 from events.event_operations import notify_new_event
 from events.tasks import check_events
+from typing import Dict
 
 """
 The bot listens for the on_ready event and then calls check_events in tasks.py
@@ -12,12 +13,24 @@ The bot will listen for the on_scheduled_event_create event and then call notify
 setup_event_events is used so that all event events can be loaded at once. instead of individually.
 """
 
-def setup_event_events(bot: commands.Bot) -> None:
+def setup_event_events(bot: commands.Bot, data: Dict[str, Dict[str, str]]) -> None:
     @bot.event
     async def on_ready():
         logging.info(f"Logged in as {bot.user.name} ({bot.user.id})")
         await bot.change_presence()
         logging.info(f"Starting background task for all guilds")
+        for guild in bot.guilds:
+            # Load Emoji ID Dictionary based on the guilds the bot is in
+            if guild.name == "pub-server":
+                emoji_id_mapping = data.get("public_server_emoji_dictionary")
+                break
+            elif guild.name == "priv-server":
+                emoji_id_mapping = data.get("private_server_emoji_dictionary")
+                break
+        else:
+            logging.info("Bot is not part of the expected servers with emoji dictionaries")
+            emoji_id_mapping = {}
+        
         check_events.start(bot)
         
     @bot.event
