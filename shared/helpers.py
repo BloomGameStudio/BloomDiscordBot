58f53e56de80720519e6d3ff7b41a27d52222b62
sarahtonein <joemoe1899@gmail.com>
2024-01-15 00:24:02 -0500

    helper func

---
+++ shared/helpers.py
@@ -0,0 +1,5 @@
+def get_channel_by_name(guild: int, channel_name: str) -> None:
+    for channel in guild.channels:
+        if channel.name == channel_name:
+            return channel
+    return None # If no channel with this name exists
