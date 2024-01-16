746f2d7aa2278cd3e98bab2c0ab425df5e35090a
sarahtonein <joemoe1899@gmail.com>
2024-01-16 02:33:47 -0500

    update funcs to handle multiple dicts

--- emotes/commands.py
+++ emotes/commands.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Dict
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -10,15 +11,13 @@ The function calls related to the commands are located in command_operations.py
 setup_contributor_commands is used so that all event commands can be loaded at once. instead of individually.
 """
 
-def setup_contrbitutor_commands(bot: commands.Bot, contributors: List[Dict[str, str]], emoji_id_mapping: Dict[str, str]) -> None:
+def setup_contrbitutor_commands(bot: commands.Bot, contributors: Dict[str, List[Dict[str, str]]], emoji_dicts: Dict[str, Dict[str, str]]) -> None:
     @bot.command(name='contributors')
     async def listcontributors(ctx: Context) -> None:
-        await list_contributors(ctx, emoji_id_mapping)
-
+        await list_contributors(ctx, contributors, emoji_dicts)
     @bot.command(name='remove_contributor')
     async def removecontributor(ctx: Context, user_mention: str) -> None:
-        await remove_contributor(ctx, contributors, emoji_id_mapping, user_mention)
-    
+        await remove_contributor(ctx, contributors, emoji_dicts, user_mention)
     @bot.command(name='add_contributor')
     async def addcontributor(ctx: Context) -> None:
-        await add_contributor(ctx, contributors, emoji_id_mapping, bot)
+        await add_contributor(ctx, contributors, emoji_dicts, bot)
