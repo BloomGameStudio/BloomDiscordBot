d43c01d68b408a22a5e6e5715565c2ce356e5805
sarahtonein <joemoe1899@gmail.com>
2024-01-22 00:31:09 -0500

    add in other consts

--- shared/constants.py
+++ shared/constants.py
@@ -7,3 +7,25 @@ GOVERNANCE_TALK_CHANNEL_ID = "🐬│governance"
 new_proposal_emoji = "💡"
 CONTRIBUTORS_FILE_PATH = "./emotes/contributors.json"
 POSTED_EVENTS_FILE_PATH = "./events/posted_events.json"
+
+
+#OK BLOOMER CONSTANTS
+
+DISCORD_ROLE_TRIGGERS = [
+	{'name':'Client','emoji_id':737603507795787816,'role':'Client Pod'},
+	{'name':'Graphics & Design','emoji_id':980752213347549234,'role':'Graphics Pod'},
+	{'name':'Backend','emoji_id':846911453839228938,'role':'Backend Pod'},
+	{'name':'Gameplay & Story Pod','emoji_id':961338498525306980,'role':'Gameplay & Story Pod'},
+	{'name':'Operations','emoji_id':945177584768004127,'role':'Policy & Ops Pod'},
+	{'name':'Financial','emoji_id':887872297082449970,'role':'Donator'},
+]
+
+ROLE_WHEN_NEW_USER_CONFIRMED='contributors'
+
+#-----------------------------#
+#THESE ARE BOT/GUILD SPECIFIC #
+#-----------------------------#
+COLOR_CHANGING_ROLE = 'bloom-visual'
+COMMAND_TRIGGERS = ['!']
+PRICE_TRIGGERS = ['$']
+GUILD_TRIGGER = 'Bloom Collective'
