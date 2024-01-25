a3b8fd1835100d12d097cef0c856f1b18aada164
sarahtonein <joemoe1899@gmail.com>
2024-01-24 21:45:32 -0500

    class addition

--- main.py
+++ main.py
@@ -10,44 +10,52 @@ from events.events import setup_event_events
 from events.event_operations import load_posted_events
 from shared.constants import CONTRIBUTORS_FILE_PATH, new_proposal_emoji
 from shared.events import setup_shared_events
-from typing import Dict, Any, List
 
 
-def main():
-    # Load contributors and emoji ID mapping from contributors.json
-    with open(CONTRIBUTORS_FILE_PATH, "r") as json_file:
-        data: Dict[str, Any] = json.load(json_file)
-        contributors: Dict[str, List[Dict[str, str]]] = {
-            "Bloom Studio": data["servers"]["Bloom Studio"]["contributors"],
-            "Bloom Collective": data["servers"]["Bloom Collective"]["contributors"],
-        }
-        emoji_dicts: Dict[str, Dict[str, str]] = {
-            "Bloom Studio": data["servers"]["Bloom Studio"]["emoji_dictionary"],
-            "Bloom Collective": data["servers"]["Bloom Collective"]["emoji_dictionary"],
-        }
-    # Discord Config
-    intents: discord.Intents = discord.Intents.default()
-    intents.message_content = True
-    intents.reactions = True
-    intents.members = True
-    bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
+class Bot:
+    def __init__(self):
+        self.bot = None
+        self.contributors = None
+        self.emoji_dicts = None
+        self.data = None
 
-    # Setup the governance discord commands, and events
-    setup_gov_commands(bot)
+    def load_data(self):
+        with open(CONTRIBUTORS_FILE_PATH, "r") as json_file:
+            self.data = json.load(json_file)
+            self.contributors = {
+                "Bloom Studio": self.data["servers"]["Bloom Studio"]["contributors"],
+                "Bloom Collective": self.data["servers"]["Bloom Collective"]["contributors"],
+            }
+            self.emoji_dicts = {
+                "Bloom Studio": self.data["servers"]["Bloom Studio"]["emoji_dictionary"],
+                "Bloom Collective": self.data["servers"]["Bloom Collective"]["emoji_dictionary"],
+            }
 
-    # Setup the emotes discord commands, and events
-    setup_contrbitutor_commands(bot, contributors, emoji_dicts)
-    # Setup the shared events
-    setup_shared_events(bot, data, proposals, new_proposal_emoji)
-    bot.posted_events = load_posted_events()
+    def setup_bot(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.reactions = True
+        intents.members = True
+        self.bot = commands.Bot(command_prefix="!", intents=intents)
 
-    # Setup the event discord commands, and events
-    setup_event_commands(bot)
-    setup_event_events(bot)
+    def setup_commands_and_events(self):
+        setup_gov_commands(self.bot)
+        setup_contrbitutor_commands(self.bot, self.contributors, self.emoji_dicts)
+        setup_shared_events(self.bot, self.data, proposals, new_proposal_emoji)
+        self.bot.posted_events = load_posted_events()
+        setup_event_commands(self.bot)
+        setup_event_events(self.bot)
 
-    # Run the bot
-    bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+    def run(self):
+        self.bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+
+    def main(self):
+        self.load_data()
+        self.setup_bot()
+        self.setup_commands_and_events()
+        self.run()
 
 
 if __name__ == "__main__":
-    main()
+    runner = Bot()
+    runner.main()
