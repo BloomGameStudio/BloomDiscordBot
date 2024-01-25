19c4000dcf188b9b21021dafa94b405410bd6ced
sarahtonein <joemoe1899@gmail.com>
2024-01-25 01:32:49 -0500

    helper class and method

--- shared/helpers.py
+++ shared/helpers.py
@@ -6,3 +6,9 @@ def get_channel_by_name(guild: discord.Guild, channel_name: str) -> discord.Text
         if isinstance(channel, discord.TextChannel) and channel.name == channel_name:
             return channel
     return None  # If no channel with this name exists
+
+# -*- coding: UTF-8 -*-
+
+class bold(str):
+    def __new__(cls, content):
+        return super().__new__(cls, '**' + content + '**')
