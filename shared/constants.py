0099a17ce527d08f0f8c8d4802d71e5f21eb3136
sarahtonein <joemoe1899@gmail.com>
2024-01-29 18:03:24 -0500

    remove ok bloomer from shared consts

--- shared/constants.py
+++ shared/constants.py
@@ -8,24 +8,3 @@ INTRODUCE_YOURSELF_CHANNEL = "🌊│introduce-yourself"
 new_proposal_emoji = "💡"
 CONTRIBUTORS_FILE_PATH = "./emotes/contributors.json"
 POSTED_EVENTS_FILE_PATH = "./events/posted_events.json"
-
-# This is an example message ID for a message of your choosing.
-# Is used in shared/events.py and shared/event_operations.py
-# Please reference those files for details on thse functions.
-RULES_MESSAGE_ID = 1201371938761748541
-
-# OK Bloomer ROLE_TRIGGERS:
-DISCORD_ROLE_TRIGGERS = [
-	{'name':'Client','emoji_id': 1199583728129802322 ,'role':'Client Pod'},
-	{'name':'Graphics & Design','emoji_id':980752213347549234,'role':'Graphics Pod'},
-	{'name':'Backend','emoji_id':846911453839228938,'role':'Backend Pod'},
-	{'name':'Gameplay & Story Pod','emoji_id':961338498525306980,'role':'Gameplay & Story Pod'},
-	{'name':'Operations','emoji_id':945177584768004127,'role':'Policy & Ops Pod'},
-	{'name':'Financial','emoji_id':887872297082449970,'role':'Donator'},
-]
-
-PRICE_CHANGE_NOMINAL_KEY = 'price_change_24h'
-PRICE_CHANGE_PERCENT_KEY = 'price_change_percentage_24h'
-
-# OK Bloomer default role
-ROLE_WHEN_NEW_USER_CONFIRMED='contributors'
