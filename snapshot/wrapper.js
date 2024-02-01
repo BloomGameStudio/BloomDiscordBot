7476111bdf7c5f4baad8c1fdbdce0f301ed36c42
sarahtonein <joemoe1899@gmail.com>
2024-01-31 21:42:21 -0500

    Snapshot doc

--- snapshot/wrapper.js
+++ snapshot/wrapper.js
@@ -1,3 +1,18 @@
+/**
+ * This is a wrapper for the createProposal function defined in snapshot.js.
+ * It reads the proposal details from the command line arguments and passes them to createProposal.
+ * 
+ * The command line arguments are expected to be in the following order:
+ * 1. Title of the proposal
+ * 2. Abstract of the proposal
+ * 3. Background of the proposal
+ * 4. Choices for the proposal (arbitrary number of choices can be provided)
+ * 
+ * This wrapper is called from the gov/proposals.py Python script using the subprocess.run function.
+ * The arguments are passed in the following order: ["node", "./snapshot/wrapper.js", title, abstract, background, "Yes", "No", "Abstain"]
+ * 
+ * If the createProposal function throws an error, the error is logged and the process exits with a status code of 1.
+ */
 const createProposal = require('./snapshot.js');
 
 const title = process.argv[2];
