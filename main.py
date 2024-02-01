392528845087eb79fb2e4fd60720b6ce7e18357f
sarahtonein <joemoe1899@gmail.com>
2024-02-01 17:16:42 -0500

    simplify main.py

--- main.py
+++ main.py
@@ -15,27 +15,21 @@ from shared.events import setup_shared_events
 The Bot class contains the bot's functionality. It has methods to load data from a JSON file, 
 set up the bot with the necessary intents, set up commands and events, and runs the bot.
 
-The JSON file located at emotes/contributors.json contains information about contributors, and emoji dictionaries
+The JSON file located at emotes/contributors.json contains a list of contributors, and emoji_id => UID dictionaries
 for two servers: 
 "Bloom Studio" and "Bloom Collective". This data is loaded into the Bot instance when it's created.
 
 The bot uses the following Discord Intents : message content, reactions, and members.
 
-The bot has several commands and events, which are set up in the `setup_commands_and_events` method. 
+The bot has several commands and events, which are set up in the main method. 
 These commands and events are defined in separate modules, which are imported at the tope of this code.
 Refer to the specific files for more information.
 
 """
 
 class Bot:
-    def __init__(self):
-        # Variables to be used by the bot instance
-        self.bot = None
-        self.contributors = None
-        self.emoji_dicts = None
-        self.data = None
 
-    def load_data(self):
+    def main(self):
         # Load the contributor and dictionary data from the JSON file emotes/contributors.json
         with open(CONTRIBUTORS_FILE_PATH, "r") as json_file:
             self.data = json.load(json_file)
@@ -54,7 +48,6 @@ class Bot:
                 ],
             }
 
-    def setup_bot(self):
         # Setup the bot with intents
         intents = discord.Intents.default()
         intents.message_content = True
@@ -62,7 +55,6 @@ class Bot:
         intents.members = True
         self.bot = commands.Bot(command_prefix="!", intents=intents)
 
-    def setup_commands_and_events(self):
         # Setup commands and events for the bot 
         setup_gov_commands(self.bot)
         setup_contrbitutor_commands(self.bot, self.contributors, self.emoji_dicts)
@@ -71,18 +63,9 @@ class Bot:
         setup_event_commands(self.bot)
         setup_event_events(self.bot)
 
-    def run(self):
+        # Run the bot
         self.bot.run(os.getenv("DISCORD_BOT_TOKEN"))
-
-    def main(self):
-        # Main method to load data, setup bot, and run.
-        self.load_data()
-        self.setup_bot()
-        self.setup_commands_and_events()
-        self.run()
-
 
 if __name__ == "__main__":
     # Create an instance of the bot and run main func
-    runner = Bot()
-    runner.main()
+    Bot().main()
