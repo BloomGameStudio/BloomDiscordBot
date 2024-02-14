08c5d649aa9a0a67eafd02e92404140a04aa8ee7
sarahtonein <joemoe1899@gmail.com>
2024-02-13 20:05:57 -0500

    Help command added

---
+++ shared/commands.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from consts.constants import MENU_COPY
+
+async def setupsharedcommands(bot: commands.Bot) -> None:
+    @bot.tree.command(name="help")
+    async def help_commands(interaction: discord.Interaction):
+        """
+        List all available commands that the bot can perform.
+
+        Parameters:
+        interaction (Interaction): The interaction of the command invocation.
+        """
+        await interaction.response.send_message(MENU_COPY)
