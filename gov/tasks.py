280c25edc0f1f3c70b0905e3b328e2843718ba7e
sarahtonein <joemoe1899@gmail.com>
2024-02-20 01:04:35 -0500

    Update tasks name to be clearer

--- gov/tasks.py
+++ gov/tasks.py
@@ -1,9 +1,11 @@
 from logger.logger import logger
-from discord.ext import tasks
+from .proposals import check_ongoing_proposals
+from discord.ext import tasks, commands
+
 
 @tasks.loop(minutes=5)
-async def ongoing_proposals_checker():
+async def concluded_proposals_task(bot: commands.Bot):
     try:
-        pass
+        await check_ongoing_proposals(bot)
     except Exception as e:
         logger.error(f"An error occurred while checking ongoing proposals: {e}")
