a061a75d1eda4178088995ee92cb2c9bd1366fc6
sarahtonein <joemoe1899@gmail.com>
2024-02-21 01:42:26 -0500

    use single bot instance as param for events

--- events/events.py
+++ events/events.py
@@ -1,14 +1,3 @@
-"""
-Setup event events for the bot.
-
-The bot listens for the on_ready event and then starts check_events, located in tasks.py
-This task runs every hour to check for upcoming events within the next 24 hours.
-
-The bot will listen for the on_scheduled_event_create event and then invoke notify_new_event in event_operations.py
-setup_event_events is used so that all event events can be loaded at once. instead of individually.
-"""
-
-
 from logger.logger import logger
 from discord import ScheduledEvent
 from discord.ext import commands
@@ -16,24 +5,24 @@ from events.event_operations import notify_new_event
 from events.tasks import check_events
 from gov.tasks import concluded_proposals_task
 
-def setup_event_events(discord_bot, custom_bot):
-    @discord_bot.event
+def setup_event_events(bot: commands.Bot) -> None:
+    @bot.event
     async def on_ready():
         """
         Handles the on_ready event. This event is triggered when the bot has successfully connected.
         check_events is then started to check for upcoming events every hour.
         """
-        logger.info(f"Logged in as {discord_bot.user.name} ({discord_bot.user.id})")
-        await discord_bot.change_presence()
-        logger.info(f"Starting background task for all guilds")
+        logger.info(f"Logged in as {bot.user.name} ({bot.user.id})")
+        await bot.change_presence()
+        logger.info("Starting background tasks for all guilds")
         try:
-            await discord_bot.tree.sync()
+            await bot.tree.sync()
         except Exception as e:
             logger.error(e)
-        check_events.start(discord_bot, custom_bot)
-        concluded_proposals_task.start(custom_bot)
+        check_events.start(bot)
+        concluded_proposals_task.start(bot)  # Moved inside on_ready event
 
-    @discord_bot.event
+    @bot.event
     async def on_scheduled_event_create(event: ScheduledEvent) -> None:
         """
         Handles the on_scheduled_event_create event. This event is triggered when a new scheduled event is created.
@@ -43,4 +32,4 @@ def setup_event_events(discord_bot, custom_bot):
         event (ScheduledEvent): The event that was created.
         """
         logger.info(f"New scheduled event created: {event.name}")
-        await notify_new_event(custom_bot, event, event.guild_id)
+        await notify_new_event(bot, event, event.guild_id)
