cda4dfc1f5d73924acc9c4befeee2a5482023f46
sarahtonein <joemoe1899@gmail.com>
2024-02-22 23:37:58 -0500

    docstring and fmt

--- events/events.py
+++ events/events.py
@@ -5,12 +5,23 @@ from events.event_operations import notify_new_event
 from events.tasks import check_events
 from gov.tasks import concluded_proposals_task
 
+"""
+Setup event events for the bot.
+
+The bot listens for the on_ready event and then starts check_events, located in tasks.py
+This task runs every hour to check for upcoming events within the next 24 hours.
+The bot will listen for the on_scheduled_event_create event and then invoke notify_new_event in event_operations.py
+setup_event_events is used so that all event events can be loaded at once. instead of individually.
+"""
+
+
 def setup_event_events(bot: commands.Bot) -> None:
     @bot.event
     async def on_ready():
         """
         Handles the on_ready event. This event is triggered when the bot has successfully connected.
         check_events is then started to check for upcoming events every hour.
+        Concluded_proposals_task is then started to check for any proposals that have ended.
         """
         logger.info(f"Logged in as {bot.user.name} ({bot.user.id})")
         await bot.change_presence()
@@ -20,7 +31,7 @@ def setup_event_events(bot: commands.Bot) -> None:
         except Exception as e:
             logger.error(e)
         check_events.start(bot)
-        concluded_proposals_task.start(bot)  # Moved inside on_ready event
+        concluded_proposals_task.start(bot)
 
     @bot.event
     async def on_scheduled_event_create(event: ScheduledEvent) -> None:
