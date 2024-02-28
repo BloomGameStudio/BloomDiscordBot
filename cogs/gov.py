21654c6bb0cac6b593a4e756855720558460aa0c
sarahtonein <joemoe1899@gmail.com>
2024-02-27 19:33:45 -0500

    gov cogs skeleton

---
+++ cogs/gov.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from gov.proposal_buttons_view import ProposalButtonsView
+from gov.proposal_selects import PublishDraftSelect
+from gov.proposals import proposals
+
+class GovCommandsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @app_commands.command(name="vote_draft")
+    async def vote_draft(self, interaction: discord.Interaction) -> None:
+        """
+        Draft, edit, or delete a vote proposal
+
+        Parameters:
+        interaction (discord.Interaction): The interaction of the command invocation.
+        """
+        try:
+            view = ProposalButtonsView(proposals)
+            await interaction.response.send_message(
+                "Click create to create a new proposal, edit, or delete to modify an existing proposal.",
+                view=view,
+            )
+        except Exception as e:
+            await interaction.response.send_message("Couldn't access proposal data.")
+
+    @app_commands.command(name="publish_draft")
+    async def publish_draft(self, interaction: discord.Interaction) -> None:
+        """
+        Publish an existing draft proposal.
+
+        Parameters:
+        interaction (discord.Interaction): The interaction of the command invocation.
+        """
+        try:
+            view = discord.ui.View()
+            view.add_item(PublishDraftSelect(proposals, self.bot))
+            await interaction.response.send_message("Select a proposal.", view=view)
+        except Exception as e:
+            await interaction.response.send_message("Couldn't access proposal data.")
