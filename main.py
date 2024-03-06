03169d21b4fb7e0ffad0dae80d8cfcbf59bdc939
sarahtonein <joemoe1899@gmail.com>
2024-03-05 19:47:11 -0500

    load ongoing_votes in main.py

--- main.py
+++ main.py
@@ -6,29 +6,19 @@ and posted events, loads the cogs, sets up commands and events for the bot, and 
 import discord
 import os
 import asyncio
-import json
 from discord.ext import commands
 from events.events import setup_event_events
-from helpers import load_posted_events, load_contributors_and_emoji_dicts
+from helpers import load_posted_events, load_contributors_and_emoji_dicts, load_ongoing_votes
 from cogs.help import HelpCommandCog
 from cogs.contributors import ContributorCommandsCog
 from cogs.events import EventCommandsCog
 from cogs.help import HelpCommandCog
 from cogs.gov import GovCommandsCog
-from config.config import ONGOING_VOTES_FILE_PATH
 
 
 class Bot:
     async def main(self):
 
-        try:
-            with open(ONGOING_VOTES_FILE_PATH, "r") as json_file:
-                try:
-                  self.ongoing_votes = json.load(json_file)
-                except json.JSONDecodeError:
-                    self.ongoing_votes = {}
-        except FileNotFoundError:
-            self.ongoing_votes = {}
         # Setup the bot with intents
         intents = discord.Intents.default()
         intents.message_content = True
@@ -37,9 +27,9 @@ class Bot:
         self.bot = commands.Bot(command_prefix="", intents=intents)
 
         # Load the contributors, emoji dicts, and posted events
+        self.bot.ongoing_votes = load_ongoing_votes()
         self.bot.posted_events = load_posted_events()
         self.contributors, self.emoji_dicts = load_contributors_and_emoji_dicts()
-        self.bot.ongoing_votes = self.ongoing_votes
 
         # Load the cogs
         await self.bot.add_cog(HelpCommandCog(self.bot))
