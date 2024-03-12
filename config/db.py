4c8252b12bbdd15934ea6c8e8407dbbdecb75610
sarahtonein <joemoe1899@gmail.com>
2024-03-12 02:22:39 -0400

    funcs to insert / update / delete / query data from db

--- config/db.py
+++ config/db.py
@@ -1,13 +1,24 @@
 from tinydb import TinyDB, Query
 
-# Initialize the database
+# Initialize the TinyDB database
 db = TinyDB('data/db.json')
 
-def get_contributors_table():
-    return db.table('contributors')
+# Function to insert data into the database
+def insert_data(table_name, data):
+    table = db.table(table_name)
+    table.insert(data)
 
-def get_ongoing_votes_table():
-    return db.table('ongoing_votes')
+# Function to update data in the database
+def update_data(table_name, key, value, new_data):
+    table = db.table(table_name)
+    table.update(new_data, key == value)
 
-def get_posted_events_table():
-    return db.table('posted_events')
+# Function to delete data from the database
+def delete_data(table_name, key, value):
+    table = db.table(table_name)
+    table.remove(key == value)
+
+# Function to query data from the database
+def query_data(table_name, key, value):
+    table = db.table(table_name)
+    return table.search(key == value)
